708652e63c4f8b4c66813fb8e633eb3119f90405
richenberg <metal-richard@hotmail.com>
2026-07-01 18:30:10 -0300

    Workflow Fix, Single Instance Fix

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -10,6 +10,13 @@ pub mod watcher;
 
 fn main() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
+            if let Some(window) = app.get_webview_window("main") {
+                let _ = window.show();
+                let _ = window.unminimize();
+                let _ = window.set_focus();
+            }
+        }))
         .plugin(tauri_plugin_shell::init())
         .setup(|app| {
             // Check command line arguments for minimized start
